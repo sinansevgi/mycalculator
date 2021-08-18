b9093957b4282df6071d388588563ca120b2bddf
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-08-18 10:03:12 +0300

    Import calculate function to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import './App.css';
+import calculate from '../logic/calculate';
+import '../assets/App.css';
 import Display from './Display';
 import ButtonPannel from './ButtonPannel';
 
 function App() {
+  if (!calculate) {
+    return true;
+  }
   return (
     <>
       <Display />
       <ButtonPannel />
     </>
-
   );
 }
 
