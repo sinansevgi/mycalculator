8d6d301c82d2739ca252d5a78c30666129bcc0d3
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-09-09 00:45:53 +0300

    Add negative test cases

--- src/tests/calculator/operate.test.js
+++ src/tests/calculator/operate.test.js
@@ -2,24 +2,27 @@ import operate from '../../logic/operate';
 
 describe('Test each mathematical operation in operate.js', () => {
   test('it should add two numbers (+)', () => {
-    expect(operate(2, 2, '+')).toBe('4');
+    expect(operate('2', '2', '+')).toBe('4');
   });
   test('it should subtract two numbers (-)', () => {
-    expect(operate(2, 2, '-')).toBe('0');
+    expect(operate('2', '2', '-')).toBe('0');
   });
   test('it should multiply two numbers (X)', () => {
-    expect(operate(2, 2, 'X')).toBe('4');
+    expect(operate('2', '2', 'X')).toBe('4');
   });
   test('it should divide two numbers (÷)', () => {
-    expect(operate(2, 2, '÷')).toBe('1');
+    expect(operate('2', '2', '÷')).toBe('1');
   });
   test('it should return first number if operation not described', () => {
-    expect(operate(8, 2, 'a')).toBe('8');
+    expect(operate('8', '2', 'a')).toBe('8');
   });
   test('it should return string', () => {
-    expect(typeof operate(8, 2, '+')).toBe('string');
+    expect(typeof operate('8', '2', '+')).toBe('string');
   });
   test('it should not return BigInt', () => {
-    expect(typeof operate(8, 2, '+')).not.toBe('big');
+    expect(typeof operate('8', '2', '+')).not.toBe('big');
+  });
+  test('it should not divide to zero', () => {
+    expect(operate('8', '0', '÷')).toBe('0');
   });
 });
